47f2b3a7cd8b038118d5a063dcd42246801f5ae0
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-05 01:13:50 -0400

    Se creó la ruta de registro de usuarios y su controlador

---
+++ controllers/usuarioController.js
@@ -0,0 +1,29 @@
+import Usuario from "../models/Usuario.js";
+
+const registrar = async (req, res) => {
+  // Evitar registros duplicados
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email });
+
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  try {
+    const usuario = new Usuario(req.body);
+    await usuario.save();
+
+    console.log(usuario)
+
+    res.json({
+      msg: "Usuario Creado Correctamente, Revisa tu Email para confirmar tu cuenta",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {
+  registrar
+};
---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { registrar } from "../controllers/usuarioController.js";
+const router = express.Router();
+
+// Autenticación, Registro y Confirmación de Usuarios
+router.post("/", registrar); // Crea un nuevo usuario
+
+export default router;
