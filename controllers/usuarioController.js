162e167728056663ea1e4e79bef454572228f271
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-05 02:04:29 -0400

    Se creó el controlador de autenticación que genera JWT

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from "../models/Usuario.js";
 import generarId from "../helpers/generarId.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
   // Evitar registros duplicados
@@ -26,6 +27,37 @@ const registrar = async (req, res) => {
   }
 };
 
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El Usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  // Comprobar si el usuario esta confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu Cuenta no ha sido confirmada");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Comprobar su password
+  if (await usuario.comprobarPassword(password)) {
+    res.json({
+      _id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email,
+      token: generarJWT(usuario._id),
+    });
+  } else {
+    const error = new Error("El Password es Incorrecto");
+    return res.status(403).json({ msg: error.message });
+  }
+};
+
 export {
-  registrar
+  registrar,
+  autenticar
 };
