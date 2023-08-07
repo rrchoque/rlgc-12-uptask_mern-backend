3aff112dbb7fc6f71bbeb73afc747848666a0fda
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-06 23:34:00 -0400

    Se creó la acción de confirmar cuenta de usuario

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registrar, autenticar } from "../controllers/usuarioController.js";
+import { registrar, autenticar, confirmar } from "../controllers/usuarioController.js";
 const router = express.Router();
 
 // Autenticación, Registro y Confirmación de Usuarios
 router.post("/", registrar); // Crea un nuevo usuario
 router.post("/login", autenticar);
+router.get("/confirmar/:token", confirmar);
 
 export default router;
