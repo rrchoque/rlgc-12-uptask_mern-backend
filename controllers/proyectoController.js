97317cbc7b008f10885523cffc9d256c2ea3460f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-07 01:14:21 -0400

    Se creó el servicio para mostrar un proyecto

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Proyecto from "../models/Proyecto.js";
 import Usuario from "../models/Usuario.js";
 
@@ -19,6 +20,28 @@ const nuevoProyecto = async (req, res) => {
 };
 
 const obtenerProyecto = async (req, res) => {
+  const { id } = req.params;
+
+  const valid = mongoose.Types.ObjectId.isValid(id)
+
+  if (!valid) {
+    const error = new Error("No Encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  const proyecto = await Proyecto.findById(id)
+
+  if (!proyecto) {
+    const error = new Error("No Encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if ( proyecto.creador.toString() !== req.usuario._id.toString() ) {
+    const error = new Error("Acción No Válida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  res.json(proyecto);
 };
 
 const editarProyecto = async (req, res) => {
