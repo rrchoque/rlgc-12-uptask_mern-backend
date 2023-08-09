0a5288c61f964a1f7dcb629ab317cd698479da98
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-09 01:33:34 -0400

    Se creó el servicio para registrar una tarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -2,6 +2,29 @@ import Proyecto from "../models/Proyecto.js";
 import Tarea from "../models/Tarea.js";
 
 const agregarTarea = async (req, res) => {
+  const { proyecto } = req.body;
+
+  const existeProyecto = await Proyecto.findById(proyecto);
+
+  if (!existeProyecto) {
+    const error = new Error("El Proyecto no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (existeProyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes los permisos para añadir tareas");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    const tareaAlmacenada = await Tarea.create(req.body);
+    // Almacenar el ID en el proyecto
+    existeProyecto.tareas.push(tareaAlmacenada._id);
+    await existeProyecto.save();
+    res.json(tareaAlmacenada);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const obtenerTarea = async (req, res) => {
