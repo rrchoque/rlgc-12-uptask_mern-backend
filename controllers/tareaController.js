6f682ba8b09638616910d1e74d4b6a1d1e5c843e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-09 01:38:23 -0400

    Se creó el servicio para mostrar una tarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -28,6 +28,21 @@ const agregarTarea = async (req, res) => {
 };
 
 const obtenerTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  res.json(tarea);
 };
 
 const actualizarTarea = async (req, res) => {
