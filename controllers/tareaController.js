aadd413f7e50c24cb0bd26d929f62718ed9bfb3b
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-09 01:54:32 -0400

    Se creó el servicio para eliminar una tarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -76,6 +76,28 @@ const actualizarTarea = async (req, res) => {
 };
 
 const eliminarTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    const proyecto = await Proyecto.findById(tarea.proyecto);
+    proyecto.tareas.pull(tarea._id);
+    await Promise.allSettled([await proyecto.save(), await tarea.deleteOne()]);
+    res.json({ msg: "La Tarea se eliminó" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const cambiarEstado = async (req, res) => {
