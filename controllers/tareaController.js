19ef0882265ce567a94fe59f40b58c7d9240c5c3
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-09 01:28:57 -0400

    Se creo el modelo de tareas, las rutas y el controlador

---
+++ controllers/tareaController.js
@@ -0,0 +1,25 @@
+import Proyecto from "../models/Proyecto.js";
+import Tarea from "../models/Tarea.js";
+
+const agregarTarea = async (req, res) => {
+};
+
+const obtenerTarea = async (req, res) => {
+};
+
+const actualizarTarea = async (req, res) => {
+};
+
+const eliminarTarea = async (req, res) => {
+};
+
+const cambiarEstado = async (req, res) => {
+};
+
+export {
+  agregarTarea,
+  obtenerTarea,
+  actualizarTarea,
+  eliminarTarea,
+  cambiarEstado,
+};
