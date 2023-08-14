785e8043259ebf5c81ef23bdc40be16a4bc0b39e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-13 23:57:03 -0400

    Se agregó dirección del frontend en los cors de la aplicación

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.use(express.json());
 dotenv.config();
 conectarDB();
 
+// Configurar CORS
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      // Puede consultar la API
+      callback(null, true);
+    } else {
+      // No esta permitido
+      callback(new Error("Error de Cors"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 // Routing
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/proyectos", proyectoRoutes);
