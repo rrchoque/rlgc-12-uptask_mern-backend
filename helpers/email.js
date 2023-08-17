9294624c5e1fdb34ec4fef5eb4dbfd4fd1e00938
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-17 01:15:43 -0400

    Se agregó la función de enviar correo para reestablecer contraseña

--- helpers/email.js
+++ helpers/email.js
@@ -28,3 +28,35 @@ export const emailRegistro = async (datos) => {
     `,
   });
 };
+
+export const emailOlvidePassword = async (datos) => {
+  const { email, nombre, token } = datos;
+
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Información del email
+
+  const info = await transport.sendMail({
+    from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com>',
+    to: email,
+    subject: "UpTask - Reestablece tu Password",
+    text: "Reestablece tu Password",
+    html: `<p>Hola: ${nombre} has solicitado reestablecer tu password</p>
+
+    <p>Sigue el siguiente enlace para generar un nuevo password: 
+
+    <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Password</a>
+    
+    <p>Si tu no solicitaste este email, puedes ignorar el mensaje</p>
+    
+    
+    `,
+  });
+};
